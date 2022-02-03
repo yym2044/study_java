49001c75e6918fa1d9bcb16c7b248af13ec6c8c0
yym2044 <yym2044@gmail.com>
2022-02-03 16:48:29 +0900

    .

---
+++ src/com/google/lecture/ch02/Hello.java
@@ -0,0 +1,15 @@
+package com.google.lecture.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 1;
+		int b = 2;
+		int c = 0;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
