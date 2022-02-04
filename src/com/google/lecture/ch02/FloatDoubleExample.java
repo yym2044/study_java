137b4f5dccb0c1b2fc960254138717279b97d1e4
yym2044 <yym2044@gmail.com>
2022-02-04 18:17:15 +0900

    .

--- src/com/google/lecture/ch02/FloatDoubleExample.java
+++ src/com/google/lecture/ch02/FloatDoubleExample.java
@@ -3,8 +3,27 @@ package com.google.lecture.ch02;
 public class FloatDoubleExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double var1 = 3.14;
+		float var3 = 3.14F;
+		
+		double var4 = 0.1234567890123456789;
+		float var5 = 0.1234567890123456789F;
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var3: " + var3);
+		System.out.println("var4: " + var4);
+		System.out.println("var5: " + var5);
+		
+		int var6 = 3000000;
+		double var7 = 3e6;
+		float var8 = 3e6F;
+		double var9 = 2e-3;
+		
+		System.out.println("var6: " + var6);
+		System.out.println("var7: " + var7);
+		System.out.println("var8: " + var8);
+		System.out.println("var9: " + var9);
 	}
 
 }
