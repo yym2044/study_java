45bdae1f56b9d94793d147801a4ea6a43606275f
yym2044 <yym2044@gmail.com>
2022-02-10 08:09:03 +0900

    .

---
+++ src/ch03/DenyLogicOperatorExample.java
@@ -0,0 +1,20 @@
+package ch03;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+
+		boolean play = true;
+		boolean play1 = false;
+		
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+	}
+
+}
---
+++ src/ch03/IncreaseDecreaseOperatorExample2.java
@@ -0,0 +1,35 @@
+package ch03;
+
+public class IncreaseDecreaseOperatorExample2 {
+
+	public static void main(String[] args) {
+
+//		++x; : 피연산자를 1 증가시키고 다른 연산 수행
+//		x++; : 다른 연산 수행 후 피연산자 1 증가
+//		다른 연산자가 없으면 앞에 있나 뒤에 있나 결과는 동일하다
+		
+		int x = 10;
+		int y = 10;
+		int a = 0;
+		int b = 0;
+		
+		++x;
+		System.out.println(x);
+		
+		y++;
+		System.out.println(y);
+		
+		a = ++x;	// x = 12
+					// x = 12
+		b = x++;	// x = 12
+					// x = 13
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(x);
+		
+		
+	
+	}
+
+}
