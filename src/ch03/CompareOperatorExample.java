90f5d2f691ce2bd037ac5c85d03d6af3c64d788b
yym2044 <yym2044@gmail.com>
2022-02-10 14:08:26 +0900

    .

---
+++ src/ch03/CompareOperatorExample.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class CompareOperatorExample {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ src/ch03/StringConcatExample.java
@@ -0,0 +1,23 @@
+package ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		
+		String str1 = "JDK " + 1.8;
+		String str2 = str1 + "입니다.";
+		
+		System.out.println("str2 : " + str2);
+		
+		String strA = "윤영민";
+		String strB = "01012345678";
+		String strC = strA + " : " + strB;
+		
+		// 이름 : 전화번호
+		//
+		
+		System.out.println("strC : " + strC);
+		
+	}
+
+}
