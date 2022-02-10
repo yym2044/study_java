bec6dd615265800fb0b4ea1fd7e5332cb633d62f
yym2044 <yym2044@gmail.com>
2022-02-10 16:00:15 +0900

    .

--- src/ch03/LogicalOperatorExample.java
+++ src/ch03/LogicalOperatorExample.java
@@ -4,8 +4,32 @@ public class LogicalOperatorExample {
 	
 	public static void main(String[] args) {
 		
+
+//		|| = | 같은 거
 		
+		int charCode = 'A';
 		
+		if((charCode>=65) & (charCode<=90)) {
+			System.out.println("대문자이군요");
+		}
+		
+		if((charCode>=97) && (charCode<=122)) {  	//&& = & 같은 거
+			System.out.println("소문자이군요");
+		}
+		
+		if(!(charCode<48) && !(charCode>57)) {
+			System.out.println("0~9 숫자이군요");
+		}
+		
+		int value = 6;
+		
+		if( (value%2==0) | (value%3==0) ) {
+			System.out.println("2 또는 3의 배수이군요");
+		}
+		
+		if( (value%2==0) || (value%3==0) ) {
+			System.out.println("2 또는 3의 배수이군요");
+		}
 	}
 
 }
