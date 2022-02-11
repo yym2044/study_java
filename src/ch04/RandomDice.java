fe70b2e443073d6d40c6efdc206fe10b9fb0ade8
yym2044 <yym2044@gmail.com>
2022-02-11 16:51:10 +0900

    .

---
+++ src/ch04/RandomDice.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class RandomDice {
+
+	public static void main(String[] args) {
+
+		int dice1 = (int) (Math.random() * 6 + 1);
+		int dice2 = (int) (Math.random() * 6 + 1);
+		
+		System.out.println("첫번째 주사위는 " + dice1 + "번 입니다.");
+		System.out.println("두번째 주사위는 " + dice2 + "번 입니다.");
+		System.out.println("두수의 합은 " + (dice1 + dice2) + "입니다.");
+		
+	}
+
+}
