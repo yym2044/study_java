dec149fb0e6523f7f1ddb0ceed03f0b187f47253
yym2044 <yym2044@gmail.com>
2022-02-11 17:12:47 +0900

    .

--- src/ch04/ForPrintFrom1To10Example.java
+++ src/ch04/ForPrintFrom1To10Example.java
@@ -1,10 +1,15 @@
 package ch04;
+
 public class ForPrintFrom1To10Example {
+	
 	public static void main(String[] args) {
+		
 		int sum=0;
 		for(int i=1;i<=10;i++) {
 			sum += i;
 		}
+		
 		System.out.println("sum = " + sum);
+		
 	}
 }
