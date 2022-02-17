07dae23e686e8bafb5468fd973c2fae800b93f20
yym2044 <yym2044@gmail.com>
2022-02-17 17:26:51 +0900

    .

---
+++ src/com/google/lecture/ch06/Member.java
@@ -0,0 +1,21 @@
+package com.google.lecture.ch06;
+
+public class Member {
+
+//	field
+	String name = "";
+	String nameEng = "";
+	String id = "";
+	String pwd = "";
+	String gender;
+	String dob = "";
+	String email = "";
+	String phone = "";
+	String address = "";
+	
+	
+//	method
+	
+	
+	
+}
---
+++ src/com/google/lecture/ch06/Products.java
@@ -0,0 +1,14 @@
+package com.google.lecture.ch06;
+
+import java.util.Date;
+
+public class Products {
+
+	String name;
+	Integer category;
+	Date production_date;
+	int price;
+	int delivery_fee;
+	String desc;
+	
+}
