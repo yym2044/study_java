996e330d3ebefb1ec9cc8e01b6ae4f53df7785fb
yym2044 <yym2044@gmail.com>
2022-02-26 22:44:05 +0900

    .

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -1,15 +1,12 @@
 package codingTest;
 
-
 public class Main {
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
+		boolean[] ch = new boolean[5];
+		
+		System.out.println(ch[0]);
+		
+	}
 
-    	double a = 1.9;
-    	
-    	int b = (int)a;
-    	
-    	System.out.println(b);
-    	
-    }
 }
--- src/codingTest/step6/Main4673.java
+++ src/codingTest/step6/Main4673.java
@@ -5,13 +5,34 @@ public class Main4673 {
 	public static void main(String[] args) {
 
 		
+		boolean[] isNotSelfNumber = new boolean[10001];		// for문에서 1~10000까지 돌릴거기 때문에 크기 10001짜리 생성, index 0은 사용안함
+		
+		for (int i = 1; i <= 10000; i++) {
+			if(d(i)<=10000) {
+				isNotSelfNumber[d(i)] = true;
+			}
+		}
+
+		for(int i=1;i<=10000;i++) {
+			if(isNotSelfNumber[i]==false) {
+				System.out.println(i);
+			}
+		}
+		
 	}
 
+//	d(n) 함수
+
 	public static int d(int n) {
-		
-		int result=0;
-		
+
+		int result = n;
+
+		while (n != 0) {
+			result += n % 10;
+			n /= 10;
+		}
+
 		return result;
 	}
-	
+
 }
