f4ec75b4ad0edd8009b93e5eaede6431d0f5ad62
yym2044 <yym2044@gmail.com>
2022-02-28 17:28:17 +0900

    .

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -1,27 +1,29 @@
 package codingTest;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];	// 입력받는 배열
+		int[] arr42 = new int[10];  // 나머지 저장 배열
 		
-		int[] arr = new int[9];
-		
-		int max = -1;
-		int index = 0;
-		
-		for(int i=0;i<9;i++) {
+		for(int i=0;i<arr.length;i++) {
+			
 			arr[i] = sc.nextInt();
-			if(max < arr[i]) {
-				max = arr[i];
-				index = i+1;
-			}
+			arr42[i] = arr[i]%42;
 		}
 		
-		System.out.println(max);
-		System.out.println(index);
 		
+		Arrays.sort(arr42);
+		
+		int count = 0;
+		int index = 0;
+		
+		
+		
+		System.out.println(count);
 	}
 }
