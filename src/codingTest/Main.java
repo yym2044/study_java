21c0b42a4a0ee5cfa4a65cb55a61953b2c3a009a
yym2044 <yym2044@gmail.com>
2022-02-28 10:23:40 +0900

    .

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -1,16 +1,28 @@
 package codingTest;
 
-public class Main{
+import java.util.Scanner;
+
+public class Main {
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+
+		int N = sc.nextInt();
+		int copy = N;
 		
-		int a = '1'-48;
+		int count = 0;
 		
+		while(true) {
+			
+			N = ((N%10)+((N/10)%10))%10 + (N%10)*10;
+			count++;
+			
+			if(copy == N)
+				break;
+			
+		}
 		
-		
-		System.out.println(a);
-		
-		char A = (char)1678;
-		
-		System.out.println(A); 
+		System.out.println(count);
 	}
 }
---
+++ src/codingTest/Main2.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+
+public class Main2 {
+	public static void main(String[] args) {
+
+
+		int N = 1 + 2 - '0';
+		
+		System.out.println(N);
+		
+	}
+}
