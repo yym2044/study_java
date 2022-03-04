88317b35b47f4199d96636350f49bf4757a6039b
yym2044 <yym2044@gmail.com>
2022-03-04 14:13:41 +0900

    .

---
+++ src/com/google/lecture/ch07/Dog.java
@@ -0,0 +1,13 @@
+package com.google.lecture.ch07;
+
+public class Dog extends Animal {
+	public Dog() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("으르렁");
+	}
+
+}
