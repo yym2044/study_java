aedd6112a3d323a46e7bbee9c86d3b3d9582faa4
yym2044 <yym2044@gmail.com>
2022-03-04 18:02:04 +0900

    .

--- src/codingTest/step8/Main2839.java
+++ src/codingTest/step8/Main2839.java
@@ -3,27 +3,12 @@ package codingTest.step8;
 import java.util.Scanner;
 
 public class Main2839 {
-
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
 		
-		sc.close();
 		
-		int bag = 0;
-
-//		N = 5X + 3Y
-
-		
-		if((N/5)%3==0) {
-			bag = bag + (N/5);
-			N /= 5;
-		} else
-		
-		System.out.println(N);
-		System.out.println(bag);
 	}
-
 }
