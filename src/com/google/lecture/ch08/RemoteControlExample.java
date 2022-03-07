b3a4283edaa39be88a4dbbdd0f33a0b0df2552f3
yym2044 <yym2044@gmail.com>
2022-03-07 17:01:51 +0900

    .

--- src/com/google/lecture/ch08/RemoteControlExample.java
+++ src/com/google/lecture/ch08/RemoteControlExample.java
@@ -1,6 +1,24 @@
 package com.google.lecture.ch08;
 
 public class RemoteControlExample {
+	
+	public static void main(String[] args) {
 		
+		Audio audio = new Audio();
+//		Television television = new Television();
+		
+		audio.turnOn();
+		audio.turnOff();
+		
+//		인터페이스를 구현한 클래스를 사용해야 할 때
+//		RemoteControl remoteControl;	// 인터페이스 참조변수 선언
+//		remoteControl = new Audio();	// 구현객체로 초기화
+		
+		RemoteControl remoteControl = new Audio();
+		
+		remoteControl.turnOn();
+		remoteControl.turnOff();
+		
+	}
 		
 }
