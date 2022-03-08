9bf9eb245252f15a65b8e538a3140774ed6c884d
yym2044 <yym2044@gmail.com>
2022-03-08 17:06:22 +0900

    .

---
+++ src/com/google/lecture/ch08/RemoteControlExample3.java
@@ -0,0 +1,9 @@
+package com.google.lecture.ch08;
+
+public class RemoteControlExample3 {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
---
+++ src/com/google/lecture/ch08/Searchable.java
@@ -0,0 +1,5 @@
+package com.google.lecture.ch08;
+
+public interface Searchable {
+	void search(String url);
+}
