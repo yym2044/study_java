ab093c4505bd4c87905a3f2624c0162c82d45486
yym2044 <yym2044@gmail.com>
2022-03-10 15:45:50 +0900

    .

--- src/codingTest/Main.java
+++ src/codingTest/Main.java
@@ -1,20 +1,12 @@
 package codingTest;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-
-		int M = sc.nextInt();
-		int N = sc.nextInt();
+		int[] arr = new int[5];
 		
-		boolean[] isPrimeNum = new boolean[N-M+1];
-		
-		
-		
+		arr[6] = 2;
 		
 	}
 
