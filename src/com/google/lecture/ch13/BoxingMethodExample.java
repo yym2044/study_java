2b4dc7d21c2ed57a9bbd801ae6be700449a89dd4
yym2044 <yym2044@gmail.com>
2022-03-15 16:44:44 +0900

    .

--- src/com/google/lecture/ch13/BoxingMethodExample.java
+++ src/com/google/lecture/ch13/BoxingMethodExample.java
@@ -4,7 +4,7 @@ public class BoxingMethodExample {
 
 	public static void main(String[] args) {
 		
-//		Box<String> box = new Box<String>();
+//		Box<Integer> box = new Box<Integer>();
 
 		Box<Integer> box = Util.<Integer>boxing(100);
 		int rtInt = box.getT();
