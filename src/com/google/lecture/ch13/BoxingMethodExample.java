d437942e83590198486b4496030af3e408cc0e42
yym2044 <yym2044@gmail.com>
2022-03-15 16:09:52 +0900

    .

---
+++ src/com/google/lecture/ch13/BoxingMethodExample.java
@@ -0,0 +1,20 @@
+package com.google.lecture.ch13;
+
+public class BoxingMethodExample {
+
+	public static void main(String[] args) {
+		
+//		Box<String> box = new Box<String>();
+
+		Box<Integer> box = Util.<Integer>boxing(100);
+		int rtInt = box.getT();
+		
+		System.out.println(rtInt);
+		
+		Box<String> box2 = Util.<String>boxing("Tony");
+		String rtString = box2.getT();
+		
+		System.out.println(rtString);
+	}
+
+}
---
+++ src/com/google/lecture/ch13/Util.java
@@ -0,0 +1,9 @@
+package com.google.lecture.ch13;
+
+public class Util {
+	public static <T> Box<T> boxing(T t) {
+		Box<T> box = new Box<T>();
+		box.setT(t);
+		return box;
+	}
+}
