03e800044d1a20aa6c8e9864016741bb1220d8f3
yym2044 <yym2044@gmail.com>
2022-03-18 10:21:05 +0900

    .

--- src/com/google/lecture/ch15/MapCollection/HashMap/HashMapExample.java
+++ src/com/google/lecture/ch15/MapCollection/HashMap/HashMapExample.java
@@ -2,6 +2,7 @@ package com.google.lecture.ch15.MapCollection.HashMap;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -34,6 +35,27 @@ public class HashMapExample {
 		//객체 삭제
 		map.remove("홍길동");
 		System.out.println("총 Entry 수: " + map.size());
+		
+		System.out.println("---------------------------------");
+		
+		//LinkedHashMap --> 순서
+		Map<String, Integer> map2 = new LinkedHashMap<String, Integer>();
+		
+		//객체 저장
+		map2.put("신용재", 85);
+		map2.put("홍길동", 90);
+		map2.put("홍장군", 80);
+		map2.put("홍길동", 95);
+		
+		//객체를 하나씩 처리
+		Set<String> keySet2 = map2.keySet();
+		Iterator<String> keyIterator2 = keySet2.iterator();
+		while(keyIterator2.hasNext()) {
+			String key = keyIterator2.next();
+			Integer value = map2.get(key);
+			System.out.println("\t" + key + " : " + value);
+		}
+		System.out.println();
 		
 	}
 }
